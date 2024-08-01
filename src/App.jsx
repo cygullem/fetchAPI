bea742043056289f209136dce7ccd54b0814dda0
cygullem <crmchs.gullem.clydecyril@gmail.com>
2024-08-01 20:13:45 +0800

    fetch api from dapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,39 @@
 import React, { useState, useEffect } from "react";
 
-function Joke() {
-  const [joke, setJoke] = useState(null);
+function App() {
+  const [client, setClients] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getClients = async () => {
+    const response = await fetch("http://localhost:5241/api/ClientApi/GetClients");
+    const result = await response.json()
+    setClients(result);
+    setLoading(false);
+  }
 
   useEffect(() => {
-    fetch("https://api.balldontlie.io/v1/teams", {
-      method: "GET",
-      headers: {
-        Authorization: "374aaa89-d6be-4e2d-a597-9471b92949ce",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setJoke(data.data);
-        console.log(data);
-      })
-      .catch((error) => console.log(error));
+    getClients();
   }, []);
 
-  if (joke == null) return <h1>Loading...</h1>;
+  if (loading) return <center><h1 className="text-3xl font-semibold">Loading...</h1></center>
 
   return (
     <>
-      <ul className="list-none">
-        {joke.map((a, i) => (
-          <li className="list-none" key={i}>
-            <p>{a.name}</p>
-          </li>
-        ))}
+     <h1 className="text-5xl font-semibold mb-4">CLIENTS</h1>
+      <ul className="gridContainer list-none grid grid-cols-2 gap-4 overflow-auto w-full">
+        {
+          client.map((c) =>
+            <li key={c.id} className="text-3xl font-semibold">
+              <div className="flex flex-row p-8 rounded-lg bg-gray-400 mt-2 gap-1">
+                <div className="bg-slate-700 text-yellow-300 p-8 rounded-lg">{c.clientName}</div>
+                <div className="bg-gray-500 p-8 rounded-lg flex-1">{c.residency}</div>
+              </div>
+            </li> 
+          )
+        }
       </ul>
     </>
   );
 }
 
-export default Joke;
+export default App;
